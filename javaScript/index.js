a41b2ccd59c675a381941a6c5b9a0edcab27328c
Only-Czesio <marek@kamyk.me>
2023-08-22 16:43:11 +0200

    added rendering of submited food calories

--- javaScript/index.js
+++ javaScript/index.js
@@ -25,6 +25,22 @@ form.addEventListener("submit", (event) => {
       console.log(data);
       return;
     }
+    let sum = Number(carbs.value) + Number(protein.value) + Number(fat.value);
+    foodList.insertAdjacentHTML(
+      "beforeend",
+      `<li class="card">
+  <div>
+    <h3 class="name">${name.value}</h3>
+    <div class="calories">${sum} calories</div>
+    <ul class="macros">
+      <li class="carbs"><div>Carbs</div><div class="value">${carbs.value}g</div></li>
+      <li class="protein"><div>Protein</div><div class="value">${protein.value}g</div></li>
+      <li class="fat"><div>Fat</div><div class="value">${fat.value}g</div></li>
+    </ul>
+  </div>
+</li>`
+    );
+
     name.value = "";
     carbs.value = "";
     protein.value = "";
